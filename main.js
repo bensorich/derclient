1c7db8b9554416d510c261651cbb207e5bdcdd92
vClientmods <44509126+vClientmods@users.noreply.github.com>
2019-04-30 01:43:57 +0200

    Update main.js

--- main.js
+++ main.js
@@ -64,6 +64,20 @@ client.on("message", message => {
 
         message.channel.send({embed})
   }
+ client.on("message", (message) => {
+    if (message.content.startsWith(".ban")) {
+        // Easy way to get member object though mentions.
+        var member= message.mentions.members.first();
+        // ban
+        member.ban().then((member) => {
+            // Successmessage
+            message.channel.send(":wave: " + member.displayName + " has been successfully banned https://gfycat.com/playfulfittingcaribou :point_right: ");
+        }).catch(() => {
+             // Failmessage
+            message.channel.send("Access Denied");
+        });
+    }
+});
 })
 
 client.login(process.env.BOT_TOKEN);
