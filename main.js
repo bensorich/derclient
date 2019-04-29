4dcb37b42b39c00a7390472767b77367d9a90ec1
vClientmods <44509126+vClientmods@users.noreply.github.com>
2019-04-30 01:49:05 +0200

    Update main.js

--- main.js
+++ main.js
@@ -78,19 +78,6 @@ client.on("message", message => {
         });
     }
 });
-    if (message.content.startsWith("$kick")) {
-        // Easy way to get member object though mentions.
-        var member= message.mentions.members.first();
-        // Kick
-        member.kick().then((member) => {
-            // Successmessage
-            message.channel.send(":wave: " + member.displayName + " has been successfully kicked :point_right: ");
-        }).catch(() => {
-             // Failmessage
-            message.channel.send("Access Denied");
-        });
-    }
-});
 })
 
 client.login(process.env.BOT_TOKEN);
