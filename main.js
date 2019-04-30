7de389b8396e8a165050644cfe5c52b3abfb3a76
vClientmods <44509126+vClientmods@users.noreply.github.com>
2019-04-30 09:54:17 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const client = new Client();
 
 client.on("ready", () => { 
 
-            client.user.setPresence({ game: { name: "Coding Mode 4.5", type: 0 } })
+            client.user.setPresence({ game: { name: "Coding Mode 4.6", type: 0 } })
             client.user.setStatus("dnd")
 
 });
