bdac4e0634b12a9624d56549933e4952a15855f7
vClientmods <44509126+vClientmods@users.noreply.github.com>
2019-04-30 03:32:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const client = new Client();
 
 client.on("ready", () => { 
 
-            client.user.setPresence({ game: { name: "Coding Mode 3.8", type: 0 } })
+            client.user.setPresence({ game: { name: "Coding Mode 3.9", type: 0 } })
             client.user.setStatus("dnd")
 
 });
@@ -92,6 +92,21 @@ client.on("message", (message) => {
         });
     }
 });
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
+  if(!args[0]) return message.channel.send("no");
+  message.channel.bulkDelete(args[0]).then(() => {
+  message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
+});
+
+}
+
+module.exports.help = {
+  name: "clear"
+}
 })
 
 client.login(process.env.BOT_TOKEN);
