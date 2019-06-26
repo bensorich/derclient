62531000cfa4b5ea46124232de253343ff88508a
vClientmods <44509126+vClientmods@users.noreply.github.com>
2019-06-26 16:04:40 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,36 +3,36 @@ const client = new Client();
 
 client.on("ready", () => { 
 
-            client.user.setPresence({ game: { name: "Coding Mode 4.9", type: 0 } })
+            client.user.setPresence({ game: { name: "e!help | Beta V 0.1", type: 0 } })
             client.user.setStatus("dnd")
 
 });
 
-let prefix = ".";
+let prefix = "e!";
 
 client.on("message", message => {
 
     if(message.author.bot) return;
-    if(message.content === prefix + "cloudbot") {
+    if(message.content === prefix + "gaming") {
 
         const embed = new RichEmbed()
-        .setColor(16776960)
-        .setDescription("Ich bin der CloudBot ! Ich bin ein Beta Projekt von vClient, Modz | Leon#6913 ich werde viele Sachen können aber wir sind uns noch nicht Sicher was ich alles können soll darum bin ich ein Beta Projekt es wird ein bisschen dauern bis ich sachen kann weil vClient, Modz | Leon#6913 muss noch Coden lernen aber er hat es geschafft mich zum Leben zu erwecken LG CLoudbot")
+        .setColor(16738816)
+        .setDescription("/")
 
         message.channel.send({embed})
   }
   if(message.content === prefix + "team") {
 
         const embed = new RichEmbed()
-        .setColor(16776960)
-        .setDescription("Die Team-Info von das CloudBot-Team kommt bald!")
+        .setColor(16738816)
+        .setDescription("Die Team-Info von das TheCrew-Team kommt bald!")
 
         message.channel.send({embed})
   }
   if(message.content === prefix + "test") {
 
         const embed = new RichEmbed()
-        .setColor(16776960)
+        .setColor(16738816)
         .setDescription("/")
 
         message.channel.send({embed})
@@ -40,57 +40,42 @@ client.on("message", message => {
   if(message.content === prefix + "help") {
 
         const embed = new RichEmbed()
-        .setColor(16776960)
+        .setColor(16738816)
         .setThumbnail(message.guild.iconURL)
-        .setDescription("*.team* **Die Team-Info von das CloudBot-Team!** \n \n *.cloudbot* **Die Info über Cloudbot!**  \n \n *.test* **Test-Command!**  \n \n *.help* **Zeige das hier an!**  \n \n *.support-server* **Der Support Server!** \n \n *.invite* **Hole dir den Bot auf deinen Server!** \n \n *.kick* **Kicke einen User von deinen Server! [Nicht Verfügbar!]** \n \n *.ban* **Banne einen User von deinen Server! [Nicht Verfügbar!]**")
+        .setDescription("\n \n *e!help* **Zeige das hier an!** \n \n *e!beta-mode* **Zeige Infos zum Beta-Modus!** \n \n *e!gaming* **Zeige Infos über den Bot an!** \n \n *e!team* **Zeige die Team-Info vom THECREW TEAM an!** \n \n *e!test* **Test-Command!** \n \n *e!kick {User}* **Kicke einen User!**[Wartung!] \n \n *e!ban {User}* **Banne einen User**[Wartung!]")
 
         message.channel.send({embed})
   }
-
- if(message.content === prefix + "support-server") {
+  if(message.content === prefix + "beta-mode") {
 
         const embed = new RichEmbed()
-        .setColor(16776960)
-        .setDescription("**Du brauchst Support ?** \n *Dann komme auf den Support Server!* \n Invite : ?")
+        .setColor(16738816)
+        .setDescription("**Der Beta-Modus!\n \n In diesen Modus gibt es nur Standard Text Commands!\n \n Status Beta-Mode : Online**")
 
         message.channel.send({embed})
   }
+  const Discord = require('discord.js');
+const client = new Discord.Client();
 
- if(message.content === prefix + "invite") {
+client.login('NTU5NzM4OTMzNTQ2ODQ0MTYw.XRN7Qw.d9UiUQe6nGfl2xbSwBBtpHexZdc');
 
-        const embed = new RichEmbed()
-        .setColor(16776960)
-        .setDescription("**Du willst den Bot auf deinen Server haben?** \n *Dann hole dir den Bot auf deinen Server!* \n Invite : https://discordapp.com/oauth2/authorize?client_id=551087921038622730&scope=bot&permissions=8")
+client.on('message', message => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+  if (!message.guild) return;
 
-        message.channel.send({embed})
+  if (message.content === '.join') {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => { // Connection is an instance of VoiceConnection
+          message.reply('I have successfully connected to the channel!');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
   }
- client.on("message", (message) => {
-    if (message.content.startsWith(".ban")) {
-        // Easy way to get member object though mentions.
-        var member= message.mentions.members.first();
-        // ban
-        member.ban().then((member) => {
-            // Successmessage
-            message.channel.send(":wave: " + member.displayName + " wurde erfolgreich gebannt! https://gfycat.com/playfulfittingcaribou :point_right: ");
-        }).catch(() => {
-             // Failmessage
-            message.channel.send("Access Denied");
-        });
-    }
-});
-client.on("message", (message) => {
-    if (message.content.startsWith(".kick")) {
-        // Easy way to get member object though mentions.
-        var member= message.mentions.members.first();
-        // Kick
-        member.kick().then((member) => {
-            // Successmessage
-            message.channel.send(":wave: " + member.displayName + " wurde erfolgreich getreten! :point_right: ");
-        }).catch(() => {
-             // Failmessage
-            message.channel.send("Access Denied");
-        });
-    }
 });
 })
 
