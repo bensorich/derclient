ed5f38dd5ad21712e11d08809c47bbd11ca07ff7
vClientmods <44509126+vClientmods@users.noreply.github.com>
2019-06-26 16:18:28 +0200

    Update main.js

--- main.js
+++ main.js
@@ -76,65 +76,32 @@ client.on('message', message => {
       message.reply('You need to join a voice channel first!');
     }
   }
-});
-// Import the discord.js module
-const Discord = require('discord.js');
+const Discord = require('discord.js')
+const client = new Discord.Client()
 
-// Create an instance of a Discord client
-const client = new Discord.Client();
-
-/**
- * The ready event is vital, it means that only _after_ this will your bot start reacting to information
- * received from Discord
- */
-client.on('ready', () => {
-  console.log('I am ready!');
-});
-
-client.on('message', message => {
-  // Ignore messages that aren't from a guild
-  if (!message.guild) return;
-
-  // If the message content starts with "!kick"
-  if (message.content.startsWith('!kick')) {
-    // Assuming we mention someone in the message, this will return the user
-    // Read more about mentions over at https://discord.js.org/#/docs/main/stable/class/MessageMentions
-    const user = message.mentions.users.first();
-    // If we have a user mentioned
-    if (user) {
-      // Now we get the member from the user
-      const member = message.guild.member(user);
-      // If the member is in the guild
-      if (member) {
-        /**
-         * Kick the member
-         * Make sure you run this on a member, not a user!
-         * There are big differences between a user and a member
-         */
-        member.kick('Optional reason that will display in the audit logs').then(() => {
-          // We let the message author know we were able to kick the person
-          message.reply(`Successfully kicked ${user.tag}`);
-        }).catch(err => {
-          // An error happened
-          // This is generally due to the bot not being able to kick the member,
-          // either due to missing permissions or role hierarchy
-          message.reply('I was unable to kick the member');
-          // Log the error
-          console.error(err);
-        });
-      } else {
-        // The mentioned user isn't in this guild
-        message.reply('That user isn\'t in this guild!');
-      }
-    // Otherwise, if no user was mentioned
-    } else {
-      message.reply('You didn\'t mention the user to kick!');
+client.login(process.eynv.TOKEN)
+  
+  
+  
+client.on ( 'message', message => {
+    warprefix = "."
+    
+    if(message.content.startsWith('${preifx}ping')) {
+messsage.channel.send('ponk')
     }
-  }
-});
+    if(message.content.startsWith('${prefix}dm')) {
+    
+      message.member.send('hello wordl!\nthis is a new line!')
+    }
 
-// Log our bot in using the token from https://discordapp.com/developers/applications/me
-client.login('NTU5NzM4OTMzNTQ2ODQ0MTYw.XRN7Qw.d9UiUQe6nGfl2xbSwBBtpHexZdc');
+    if(message.content.startsWith('${prefix}say')) {
+      let args = message.content.slice(prefix.length).trim().split(/ +/g);
+      let text = args.join(" ");
+      if(!text) return message.replay('hey, say something *im giving up on u*')
+    message.channel.send(text)
+    }
+
+    })
 })
 
 client.login(process.env.BOT_TOKEN);
