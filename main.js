cf5300ee7927e21c882dfe4c171ca7be92686b0c
vClientmods <44509126+vClientmods@users.noreply.github.com>
2019-06-26 16:24:20 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const client = new Client();
 
 client.on("ready", () => { 
 
-            client.user.setPresence({ game: { name: ".help | Beta V 0.2", type: 0 } })
+            client.user.setPresence({ game: { name: ".help | Beta V 0.3", type: 0 } })
             client.user.setStatus("dnd")
 
 });
@@ -54,27 +54,6 @@ client.on("message", message => {
 
         message.channel.send({embed})
   }
-  const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.login('NTU5NzM4OTMzNTQ2ODQ0MTYw.XRN7Qw.d9UiUQe6nGfl2xbSwBBtpHexZdc');
-
-client.on('message', message => {
-  // Voice only works in guilds, if the message does not come from a guild,
-  // we ignore it
-  if (!message.guild) return;
-
-  if (message.content === '.join') {
-    // Only try to join the sender's voice channel if they are in one themselves
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          message.reply('I have successfully connected to the channel!');
-        })
-        .catch(console.log);
-    } else {
-      message.reply('You need to join a voice channel first!');
-    }
 })
 
 client.login(process.env.BOT_TOKEN);
