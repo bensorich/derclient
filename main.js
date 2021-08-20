fccc5096eabf41aa267573813de27eb78491a757
Leon <44509126+leon9700@users.noreply.github.com>
2021-08-20 17:07:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -54,6 +54,37 @@ client.on("message", message => {
 
         message.channel.send({embed})
   }
+const Discord = require('discord.js')
+
+
+module.exports.run = async (bot, message, args) => {
+
+  if (!message.member.hasPermission('KICK_MEMBERS', { checkAdmin: true, checkOwner: true })) return message.reply('You cannot use this command!') 
+  // I like to have it so people with the administrator permission can use it regardless.
+  const user = message.mentions.users.first();
+    if (user) {
+      const member = message.guild.member(user);
+      if (member) {
+        member
+          .kick('Optional reason that will display in the audit logs')
+          .then(() => {
+            message.reply(`Successfully kicked ${user.tag}`);
+          })
+          .catch(err => {
+            message.reply('I was unable to kick the member');
+            console.error(err);
+          });
+      } else {
+        message.reply("That user isn't in this guild!");
+      }
+    } else {
+      message.reply("You didn't mention the user to kick!");
+    }
+};
+
+module.exports.help = {
+  name: "kick"
+}
 })
 
 client.login(process.env.BOT_TOKEN);
